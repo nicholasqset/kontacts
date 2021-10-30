f8ec47c12cd7c3cdba647d05ec8e72c5faa9bceb
nicholas <nicholas@qset.co.ke>
2021-10-30 19:24:48 +0300

    initial commit

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <body class="container-fluid">
         <h4 class="text-center font-weight-bold">Address Book</h4>
         <button type="button" class="btn btn-primary" onclick="kontact.addKontact();">Add New Contact</button>
+               
         <div id="dv_kontacts">
             <table class="table" id="tbl_kontacts">
                 <tr>
@@ -19,7 +20,6 @@
                     <th>Street</th>
                     <th>Zip Code</th>
                     <th>City</th>
-                    <th>Edit</th>
                     <th>Delete</th>
                 </tr>
             </table>
@@ -41,28 +41,33 @@
                                 for (let [key, value] of Object.entries(response.data)) {
 //                                    console.log(key);
 //                                    console.log(value);
+                                    let purgeId = 0;
                                     let table = '<tr>'; 
+                                    table += '<td>' + key + '</td>';
                                     for (let [key2, value2] of Object.entries(value)) {
 //                                        console.log('key='+key);
 //                                        console.log('key2='+key2);
 //                                        console.log('value2='+value2);
                                         
-                                        table += '<td>' + value2 + '</td>'
+                                        if(key2 === 'id'){
+                                            purgeId = value2;
+                                        }else{
+                                            table += '<td>' + value2 + '</td>';
+                                        }
+                                        
                                     }
-                                    let editOpt = "<a href='#'>edit</a>";
-                                    let delOpt = "<a href='#'>delete</a>";
-                                    table += '<td>' + editOpt + '</td>'
-                                    table += '<td>' + delOpt + '</td>'
+                                    
+                                    let delOpt = "<a href='javascript:void(0);' onclick=\"kontact.deleteKontact("+purgeId+")\">delete</a>";
+                                    
+                                    table += '<td>' + delOpt + '</td>';
                                     table += '</tr>'; 
                                     
 //                                    console.log(table);
                                     
-                                    $('#tbl_kontacts').append(table);
-                                    
+                                    $('#tbl_kontacts').append(table);                        
                                 }
                             }else{
                                 console.log('invalid data');
-
                             }
                         },
                         error: function (error) {
@@ -71,8 +76,131 @@
                     });
                 },
                 addKontact: function(){
-                    console.log('adding...');
-                    $('#dv_kontacts').html('zzzzzzz');
+//                    console.log('adding...');
+                    
+                    let html = '';
+                    
+                    html += "<form name=\"frm_kontact\" id=\"frm_kontact\" method=\"post\" action=\"void%200\" onsubmit=\"javascript:return false;\">";
+                        html += "<div class=\"form-group\">";
+                            html += "<label for=\"txt_full_name\">Full Name</label>";
+                            html += "<input type=\"text\" class=\"form-control\" id=\"txt_full_name\" placeholder=\"Enter Full Name\" required>";
+                        html += "</div>";
+
+                        html += "<div class=\"form-group\">";
+                            html += "<label for=\"txt_first_name\">First Name</label>";
+                            html += "<input type=\"text\" class=\"form-control\" id=\"txt_first_name\" placeholder=\"Enter First Name\" required>";
+                        html += "</div>";
+
+                        html += "<div class=\"form-group\">";
+                            html += "<label for=\"txt_email\">Email address</label>";
+                            html += "<input type=\"email\" class=\"form-control\" id=\"txt_email\" placeholder=\"Enter email\" required>";
+                        html += "</div>";
+
+                        html += "<div class=\"form-group\">";
+                            html += "<label for=\"txt_street\">Street Address</label>";
+                            html += "<input type=\"text\" class=\"form-control\" id=\"txt_street\" placeholder=\"Enter Street Address\" required>";
+                        html += "</div>";
+
+                        html += "<div class=\"form-group\">";
+                            html += "<label for=\"txt_zip_code\">Zip Code</label>";
+                            html += "<input type=\"text\" class=\"form-control\" id=\"txt_zip_code\" placeholder=\"Enter Zip Code\" required>";
+                        html += "</div>";
+
+                        html += "<div class=\"form-group\">";
+                            html += "<label for=\"cmb_city\">City</label>";
+                            html += "<select class=\"form-control\" id=\"cmb_city\" required>";
+
+                            html += "</select>";
+                        html += "</div>";
+
+                        html += "<hr>";
+
+                        html += "<button type=\"button\" class=\"btn btn-primary\" onclick=\"kontact.saveKontact();\">Submit</button>";
+                        html += "&nbsp";
+                        html += "<button type=\"button\" class=\"btn btn-warning\" onclick=\"kontact.reload();\">Cancel</button>";
+                    html += "</form>";
+                    
+                    $('#dv_kontacts').html(html);
+                    
+                    kontact.getCities();
+                },
+                getCities: function(){
+                    $.ajax({
+                        url: './parser/?function=getCities',
+                        type: "POST",
+                        success: function (response) {
+                            response = JSON.parse(response);
+                            if(response.success === 1){
+                                for (let [key, value] of Object.entries(response.data)) {
+                                    let cities = '';
+                                    for (let [key2, value2] of Object.entries(value)) {
+//                                        console.log(value2);
+                                        $('#cmb_city').append('<option>'+ value2+ '</option>');
+                                    }
+                                }
+                            }else{
+                                console.log('invalid data');
+                            }
+                        },
+                        error: function (error) {
+                            console.log("error: " + JSON.stringify(error));
+                        }
+                    });
+                },
+                saveKontact: function(){
+//                    let data = $('#frm_kontact').serialize();
+//                    console.log(data);
+                    let name = $('#txt_full_name').val();
+                    let first_name = $('#txt_first_name').val();
+                    let email = $('#txt_email').val();
+                    let street = $('#txt_street').val();
+                    let zip_code = $('#txt_zip_code').val();
+                    let city = $('#cmb_city').val();
+                    
+                    let data = 'name='+name+'&first_name='+first_name+'&email='+email+'&street='+street+'&zip_code='+zip_code+'&city='+city;
+                    $.ajax({
+                        url: './parser/?function=saveKontact&'+ data,
+                        type: "POST",
+                        success: function (response) {
+                            response = JSON.parse(response);
+                            if(response.success === 1){
+                                alert('saving successfully occured');
+                                kontact.reload();
+                            }else{
+                                console.log('error');
+                                alert('error');                                
+                            }
+                        },
+                        error: function (error) {
+                            console.log("error: " + JSON.stringify(error));
+                        }
+                    });
+                },
+                deleteKontact: function(id){
+                    if(confirm('Delete?')){
+                        $.ajax({
+                            url: './parser/?function=deleteKontact&id='+ id,
+                            type: "POST",
+                            success: function (response) {
+                                response = JSON.parse(response);
+                                if(response.success === 1){
+                                    alert('deleting successfully occured');
+                                    kontact.reload();
+                                }else{
+                                    console.log('error');
+                                    alert('error');                                
+                                }
+                            },
+                            error: function (error) {
+                                console.log("error: " + JSON.stringify(error));
+                            }
+                        });
+                    }
+                },
+                reload: function(){
+                    console.log('reloading...');
+                    window.location.reload();
+
                 }
             };
         </script>
--- parser/index.php
+++ parser/index.php
@@ -38,15 +38,100 @@ class AddressBook{
 
             }
             
-            $data['data'] = $table_data;
-            $data['success'] = 1;
-            $data['message'] = 'success';
+            $data['data']       = $table_data;
+            $data['success']    = 1;
+            $data['message']    = 'success';
         }else{
-            $data['success'] = 0;
-            $data['message'] = 'no record found';
+            $data['success']    = 0;
+            $data['message']    = 'no record found';
         }
         
-//        
+        return json_encode($data);
+    }
+    
+    public function getCities(){
+        global $pdo;
+        
+        $data = array();
+        
+        $sql = 'SELECT `id`,`name` FROM `cities`';
+        $statement = $pdo->query($sql);
+        
+        if($statement->rowCount() > 0){
+            $table_data = array();
+            
+            $count = 1;
+            while (($row = $statement->fetch(PDO::FETCH_ASSOC)) !== false) {
+                $id         = $row['id'];
+                $name       = $row['name'];
+                
+//                $table_data[$count]['id'] = $id;
+                $table_data[$count]['name'] = $name;
+                
+                $count++;
+            }
+            
+            $data['data']       = $table_data;
+            $data['success']    = 1;
+            $data['message']    = 'success';
+        }else{
+            $data['success']    = 0;
+            $data['message']    = 'no record found';
+        }
+        
+        return json_encode($data);
+    }
+    
+    public function saveKontact(){
+        global $pdo;
+//        print_r($_GET);
+        $data = array();
+        
+        $name       = filter_input(INPUT_GET, 'name');
+        $first_name = filter_input(INPUT_GET, 'first_name');
+        $email      = filter_input(INPUT_GET, 'email');
+        $street     = filter_input(INPUT_GET, 'street');
+        $zip_code   = filter_input(INPUT_GET, 'zip_code');
+        $city       = filter_input(INPUT_GET, 'city');
+        
+        $sql = "INSERT INTO `address_book`("
+                . "`name`, `first_name`, `email`, `street`, `zip_code`, `city`"
+                . ") "
+                . "VALUES ("
+                . "'{$name}','{$first_name}','{$email}','{$street}','{$zip_code}','{$city}'"
+                . ")";
+                
+        $inserted = $pdo->exec($sql);
+        
+        if($inserted){
+            $data['success']    = 1;
+            $data['message']    = 'success';
+        }else{
+            $data['success']    = 0;
+            $data['message']    = 'no record found';
+        }
+        
+        return json_encode($data);
+    }
+    
+    public function deleteKontact(){
+        global $pdo;
+        $data = array();
+        
+        $id      = filter_input(INPUT_GET, 'id');
+        
+        $sql = "DELETE FROM `address_book` WHERE `id` = {$id}";
+                
+        $deleted = $pdo->exec($sql);
+        
+        if($deleted){
+            $data['success']    = 1;
+            $data['message']    = 'success';
+        }else{
+            $data['success']    = 0;
+            $data['message']    = 'no record found';
+        }
+        
         return json_encode($data);
     }
 }
@@ -56,9 +141,15 @@ $addressBook = new AddressBook();
 $function = filter_input(INPUT_GET, 'function');
 //echo 'funx='.$function;
 
-if($function == 'listContacts'){
-    
-   echo $addressBook->listContacts();
-    
+if($function == 'listContacts'){    
+   echo $addressBook->listContacts();    
 }
-
+elseif($function == 'getCities'){    
+   echo $addressBook->getCities();    
+}
+elseif($function == 'saveKontact'){    
+   echo $addressBook->saveKontact();    
+}
+elseif($function == 'deleteKontact'){    
+   echo $addressBook->deleteKontact();    
+}
