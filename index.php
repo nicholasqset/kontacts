ad6ce49086c229a364a18bad04288a0e88282f88
nicholas <nicholas@qset.co.ke>
2021-10-30 14:44:55 +0300

    initial commit

--- index.php
+++ index.php
@@ -7,12 +7,20 @@
         <link rel="stylesheet" href="assets/bootstrap/node_modules/bootstrap/dist/css/bootstrap.min.css"/>
     </head>
     <body>
+        <h4 class="text-center font-weight-bold">Address Book</h4>
         <div>
-            
+            <table class="table" id="tbl_kontacts">
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>First Name</th>
+                    <th>Email</th>
+                    <th>Street</th>
+                    <th>Zip Code</th>
+                    <th>City</th>
+                </tr>
+            </table>
         </div>
-        <?php
-        // put your code here
-        ?>
         <script src="assets/jquery/node_modules/jquery/dist/jquery.min.js"></script>
         <script type="text/javascript">
             $(function() {
@@ -27,8 +35,25 @@
                         success: function (response) {
                             response = JSON.parse(response);
                             if(response.success === 1){
-                                console.log('data in');
-//                                window.location.href = '../';
+                                for (let [key, value] of Object.entries(response.data)) {
+//                                    console.log(key);
+//                                    console.log(value);
+                                    let table = '<tr>'; 
+                                    for (let [key2, value2] of Object.entries(value)) {
+//                                        console.log('key='+key);
+//                                        console.log('key2='+key2);
+//                                        console.log('value2='+value2);
+                                        
+                                        table += '<td>' + value2 + '</td>'
+                                    }
+                                    
+                                    table += '</tr>'; 
+                                    
+                                    console.log(table);
+                                    
+                                    $('#tbl_kontacts').append(table);
+                                    
+                                }
                             }else{
                                 console.log('invalid data');
 
