8150c016d1aad9a7946704d3820be319c8f0ddd3
Jean Eric <erickenzo2@gmail.com>
2021-11-08 11:07:50 +0300

    Added Prominent disclosure

--- app/src/main/java/com/pango/pangodelivery/ui/auth/SplashActivity.kt
+++ app/src/main/java/com/pango/pangodelivery/ui/auth/SplashActivity.kt
@@ -1,15 +1,13 @@
 package com.pango.pangodelivery.ui.auth
 
 import android.Manifest
-import android.R.attr.name
-import android.R.id
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuth.AuthStateListener
 import com.google.firebase.firestore.ktx.firestore
@@ -17,6 +15,7 @@ import com.google.firebase.ktx.Firebase
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import com.pango.pangodelivery.R
+import com.pango.pangodelivery.ui.IntroSliderActivity
 import com.pango.pangodelivery.ui.MainActivity
 import es.dmoral.toasty.Toasty
 
@@ -25,60 +24,30 @@ class SplashActivity : AppCompatActivity() {
     private var mAuth: FirebaseAuth? = null
     private var mAuthListener: AuthStateListener? = null
     private val TAG = "SplashActivity"
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val settings = this.getPreferences(Context.MODE_PRIVATE)
+        if (settings.getBoolean("my_first_time", true)) {
+            //the app is being launched for first time, do something
+            Log.d("Comments", "First time");
+
+            // first time task
+            startActivity(Intent(this@SplashActivity, IntroSliderActivity::class.java))
+            finish()
+
+            // record the fact that the app has been started at least once
+            settings.edit().putBoolean("my_first_time", false).apply();
+        }
+
 
         mAuth = FirebaseAuth.getInstance()
 //        mAuth!!.signOut()
 
         val db = Firebase.firestore
-        val permissionlistener: PermissionListener = object : PermissionListener {
-            override fun onPermissionGranted() {}
-            override fun onPermissionDenied(deniedPermissions: List<String>) {
-                Toast.makeText(
-                    this@SplashActivity,
-                    "Permission Denied\n$deniedPermissions",
-                    Toast.LENGTH_SHORT
-                )
-                    .show()
-                finish()
-                moveTaskToBack(true) // finish activity
-            }
-        }
-
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            TedPermission.with(this)
-                .setPermissionListener(permissionlistener)
-                .setDeniedTitle("Permission denied")
-                .setDeniedMessage(
-                    "If you reject permission, you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]"
-                )
-                .setPermissions(
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_NETWORK_STATE,
-                    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-                    Manifest.permission.CAMERA,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-                )
-                .check()
-        } else {
-            TedPermission.with(this)
-                .setPermissionListener(permissionlistener)
-                .setDeniedTitle("Permission denied")
-                .setDeniedMessage(
-                    "If you reject permission, you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]"
-                )
-                .setPermissions(
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_NETWORK_STATE
-                )
-                .check()
-        }
 
         mAuthListener = AuthStateListener { firebaseAuth ->
             val user = firebaseAuth.currentUser
